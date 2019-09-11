7ddb10e1d51771b14b3faa71fa3a2b70642cbcab
ZCYBORG-404 <51997622+ZCYBORG-404@users.noreply.github.com>
2019-09-11 21:09:32 +0800

    Add files via upload

---
+++ i.sh
@@ -0,0 +1,48 @@
+# INI BAHAN YANG AKAN KALIAN INSTALL
+
+# HEHEHEHEHEHE
+
+# KONTAK CEWEK 081234567890
+
+
+pkg install git
+
+pkg install bash
+
+pkg install python
+
+pkg install python2
+
+pkg install
+
+apt install git
+
+pkg install fish
+
+pkg update && pkg upgrade
+
+pkg install toilet
+
+pkg install php
+
+apt update
+
+apt update
+
+apt upgrade
+
+apt install git
+
+apt install nano
+
+pkg install mc
+
+pkg install openssh
+
+pkg install lolcat
+
+pkg install bash
+
+pkg install busybox
+
+echo "SUCCES" | lolcat
